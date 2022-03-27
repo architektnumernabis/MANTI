dbe7f8d6c8304af30943816a408e0e378b3a8dbe
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-03-27 20:51:19 +0200

    added whyswim section and best swimmers sections and adjusted it to all breakpoints, started to experiment with color palletes

---
+++ static/js/slick-setting.js
@@ -0,0 +1,23 @@
+$('.carousel').slick({
+    mobileFirst: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 819,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+    ]
+  });
