e3f55b12df7655a43342930ae73187305bc92e74
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-04-02 00:38:03 +0200

    added news section, cofigured slick.js for this, next I have to make it look good on mobiles

--- static/js/slick-setting.js
+++ static/js/slick-setting.js
@@ -6,8 +6,7 @@ $('.carousel').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
 
-    responsive: [
-        {
+    responsive: [{
             breakpoint: 576,
             settings: {
                 slidesToShow: 2
@@ -28,4 +27,40 @@ $('.carousel').slick({
             }
         }
     ]
-  });
+});
+
+$('.news-carousel').slick({
+    mobileFirst: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+    responsive: [{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 819,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                arrows: true,
+                slidesToShow: 2
+            }
+        }
+    ]
+});
