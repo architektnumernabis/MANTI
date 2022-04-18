e160f35ad60bcf48fa5f119175d3694a96b14bfb
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-04-18 11:24:03 +0200

    changed navigation and added animations on header and buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,43 @@
 const nav = document.querySelector('.navbar');
-const navButton = document.querySelector('.burger-btn');
+const navButton = document.querySelector('.hamburger');
+const navButtonBox = document.querySelector('.hamburger-box');
+const navButtonInner = document.querySelector('.hamburger-inner');
 const navItems = document.querySelectorAll('.navbar__item');
+const header = document.querySelector('.header');
+const optionsForFirstObserver = {
+    rootMargin: '-20%'
+};
 
 
 
 //function that closes navigation after clicking on link or a button
 const handleNav = () => {
     nav.classList.toggle('navbar--active');
+    navButton.classList.toggle('is-active');
 
     navItems.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.remove('navbar--active');
+            navButton.classList.remove('is-active');
         })
     })
 }
 
 navButton.addEventListener('click', handleNav);
 
-//function that closes navigation after clicking outside of the navigation
-document.addEventListener('click', function (event) {
-    if (!event.target.classList.contains('navbar') && !event.target.classList.contains('burger-btn__box') && !event.target.classList.contains('burger-btn')) {
-        nav.classList.remove('navbar--active');
-    }
-})
+
+
+
+
+
+const observerOne = new IntersectionObserver(function (entries, observerOne) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            navButtonInner.classList.add('hamburger-black');
+        } else {
+            navButtonInner.classList.remove('hamburger-black');
+        }
+    })
+}, optionsForFirstObserver);
+
+observerOne.observe(header);
