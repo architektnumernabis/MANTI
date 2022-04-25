9c50544118f929d37437ee3ac8e788b5b8206da0
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-04-25 22:27:48 +0200

    fixing hero image on IOS

---
+++ static/js/is_IOS.js
@@ -0,0 +1,17 @@
+//script that changes bacground-attachment to scroll on iOS devices
+function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
+
+if (iOS() == true) {
+    document.getElementById("header").style.backgroundAttachment = "scroll"
+}
