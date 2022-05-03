bafa468f452e73e2a69c35bd8cfbaf774ce5440b
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-05-03 14:10:21 +0200

    fixed js responsible for color of burge-btn menu

--- static/js/breakpoints.js
+++
@@ -1,8 +0,0 @@
-//file containing values of options for observers depending on breakpoint
-
-if(windowWidth >= 414){
-   optionsAboutMe = {
-       rootMargin: '5% 0px 0px 0px'
-   };
-}
-
--- static/js/main.js
+++ static/js/main.js
@@ -5,75 +5,11 @@ const navButtonInner = document.querySelector('.hamburger-inner');
 const navItems = document.querySelectorAll('.navbar__item');
 const header = document.getElementById('header');
 const aboutMe = document.querySelector('.about-me');
+const whySwim = document.querySelector('.why-swim');
 const news = document.querySelector('.news');
-const windowWidth = window.innerWidth;
+var windowWidth = window.innerWidth;
 
-var optionsObserverOne = {
-    rootMargin: '-10% 0px 0px 0px'
-};
-
-var optionsAboutMe = {
-    rootMargin: '10% 0px -90% 0px',
-};
-
-const optionsNews = {
-
-};
-
-//BREAKPOINTS
-if (windowWidth > 413) {
-    optionsAboutMe = {
-        rootMargin: '5% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 575) {
-    optionsAboutMe = {
-        rootMargin: '2% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 631) {
-    optionsAboutMe = {
-        rootMargin: '15% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 739) {
-    optionsObserverOne = {
-        rootMargin: '-15% 0px 0px 0px'
-    };
-    optionsAboutMe = {
-        rootMargin: '2% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 992) {
-    optionsAboutMe = {
-        rootMargin: '5% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 1200) {
-    optionsObserverOne = {
-        rootMargin: '-20% 0px 0px 0px'
-    };
-    optionsAboutMe = {
-        rootMargin: '10% 0px 0px 0px'
-    };
-}
-
-if (windowWidth > 1400) {
-    optionsObserverOne = {
-        rootMargin: '-30% 0px 0px 0px'
-    };
-    optionsAboutMe = {
-        rootMargin: '10% 0px 0px 0px'
-    };
-}
-
-
-
+const allSections = document.querySelectorAll('.section');
 
 //function that closes navigation after clicking on link or a button
 const handleNav = () => {
@@ -90,47 +26,19 @@ const handleNav = () => {
 
 navButton.addEventListener('click', handleNav);
 
-//observer function that changes color of burger button afters crolling down from header
-const observerOne = new IntersectionObserver(function (entries, observerOne) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            navButtonInner.classList.remove('hamburger-black');
-        } else {
-            navButtonInner.classList.add('hamburger-black');
-        }
-    })
-}, optionsObserverOne);
 
-observerOne.observe(header);
+//function that changes color of burger-btn depending on section
+const buttonColorChange = () => {
+    const currentSection = window.scrollY;
 
-//observer that changes nav color after leaving about-me section
-const observerAboutMe = new IntersectionObserver(function (entries, observerAboutMe) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
+    allSections.forEach(section => {
+        if (section.offsetTop - ((0.1 * windowWidth) + 48) < currentSection && section.classList.contains('white-section')) {
             navButtonInner.classList.add('hamburger-black');
-        } else {
+            console.log(section);
+        } else if (!section.classList.contains('white-section') && section.offsetTop + 0.1*windowWidth - 48  < currentSection) {
             navButtonInner.classList.remove('hamburger-black');
         }
-    })
-}, optionsAboutMe)
+    });
+}
 
-observerAboutMe.observe(aboutMe);
-
-//
-const observerNews = new IntersectionObserver(function (entries, observerNews) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-
-        } else {
-
-        }
-    })
-}, optionsNews)
-
-observerNews.observe(news);
-
-//scripts that pops down menu items in subsite called best-swimmers
-
-//arrow -strzałka
-//navbarTitle tytul
-
+window.addEventListener('scroll', buttonColorChange);
